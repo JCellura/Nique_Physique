c07155ff5f090d8fd0c1b79450a1592dfc9812f9
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2018-10-22 02:54:19 -0400

    fixing heroku bugs

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client/build'));
+    app.use(express.static('client/public'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
     });
 
 }
