f90414f8f02e5151be0066609ed7f9a1b8711d67
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2018-10-22 04:14:13 -0400

    fixing heroku bug

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static(__dirname + 'client/build'))
+    app.use(express.static(path.join(__dirname, 'client/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    // });
 }
 
 app.post("/api/form", (req,res) => {
