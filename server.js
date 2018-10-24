f0bd217d3c9d61af6295feb4fb85bd710cded214
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2018-10-23 21:19:46 -0400

    fixing heroku bugs

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
     app.use(express.static(path.join(__dirname, 'client/build')))
 
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
-    // });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client/build', 'index.html'));
+    });
 }
 
 app.post("/api/form", (req,res) => {
