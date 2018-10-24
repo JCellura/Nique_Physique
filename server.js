f01db6789a8885fb3d1e92578d0168197142e178
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2018-10-23 21:29:04 -0400

    fixing heroku bugs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'index.html'));
+        res.sendFile(path.resolve("client/build", 'index.html'));
     });
 }
 
