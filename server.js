3177facb6899e034f541639073847cbac1c2dc7b
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2018-11-02 02:43:59 -0400

    fixing bugs

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ console.log(s3.config.niquePassword);
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 if (process.env.NODE_ENV === 'production') {
