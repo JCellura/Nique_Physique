6b1d52f37184709df3ef961132d1cc728b13e6aa
Joe Cellura <joecellura@Joes-MacBook-Air.local>
2018-12-21 07:10:35 -0500

    updated success stories drop down

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -52,11 +52,11 @@ const Navbars = props => (
             Training
           </NavItem>
           <NavDropdown eventKey={3} title="Success Stories" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
+            {/* <MenuItem eventKey={3.1}>Action</MenuItem>
             <MenuItem eventKey={3.2}>Another action</MenuItem>
             <MenuItem eventKey={3.3}>Something else here</MenuItem>
             <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            <MenuItem eventKey={3.3}>Separated link</MenuItem> */}
           </NavDropdown>
         </Nav>
         <Nav pullRight>
